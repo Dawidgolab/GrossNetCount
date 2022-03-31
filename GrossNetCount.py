b9f47f6ee2d3369e6698317581c3885f7f2c7cc3
Dawid Gołąb <dawidg122@interia.pl>
2022-03-31 12:46:11 +0200

    Add new files

---
+++ GrossNetCount.py
@@ -0,0 +1,16 @@
+# Software Development Company (Salaries)
+
+VAT = 23
+calculatedVAT = 1 + 23/100
+
+priceNetTrainee = 1600
+priceNetJunior =  3000
+priceNetMid = 5000 
+priceNetSenior = 9000 
+priceNetMaster = 15000
+
+priceGrossTrainee = priceNetTrainee * calculatedVAT
+priceGrossJunior = priceNetJunior * calculatedVAT
+priceGrossMid = priceNetMid * calculatedVAT
+priceGrossSenior = priceNetSenior * calculatedVAT
+priceGrossMaster = priceNetMaster * calculatedVAT
